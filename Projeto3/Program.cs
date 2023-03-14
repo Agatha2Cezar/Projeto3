a625f21e5a3b0147e933f9acfa8818de0479068b
curso.ads2 <curso.ads2@LAB6109>
2023-03-14 20:37:03 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*EXERCICIO 1 
+            double n1, n2, media;
+
+            Console.WriteLine("Digite uma nota: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite mais uma nota: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            media = (n1 + n2) / 2;
+            Console.WriteLine("A média das notas é " + media);
+
+            if(media >= 6)
+            {
+                Console.WriteLine("Você está aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Você está reprovado e vai para a recuperação!!");
+            }
+
+            */
+            /*
+            double valorInicial, valorFinal;
+
+            Console.WriteLine("Digite o valor do produto: ");
+            valorInicial = Convert.ToDouble(Console.ReadLine());
+            valorFinal = valorInicial * 95 / 100; //valorFinal=valorInicial*5/100
+            Console.WriteLine("O valor do produto com 5% de desconto é " +valorFinal + ",00");
+            */
+
+            //EXERCICIO gramas
+
+            double 
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
